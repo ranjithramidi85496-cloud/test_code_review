6765bf1477406fcb34c296d107a96fd67f722315
ranjithramidi <ranjithramidi125@gmail.com>
2026-07-07 10:40:59 +0530

    discount is updated

--- testt123.py
+++ testt123.py
@@ -15,7 +15,7 @@ class DiscountService:
         Apply discount to base price
         """
         # VIOLATION: Hardcoded discount rate (should use ConfigService)
-        discount = 34567
+        discount = 3456
         
         # VIOLATION: SQL Injection vulnerability
         cursor.execute("SELECT * FROM table WHERE id = ?", (user_input,))
